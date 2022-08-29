a11daedcfc3ee57f84e0f9906bad871bc5c70b0a
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2022-08-29 17:38:32 +0530

    make counter plus minus

---
+++ src/components/CounterPlusMinus.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const CounterPlusMinus = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="d-flex justify-content-center align-items-center flex-column vh-100">
+        <h1 className="common_btn bg-transparent fs-4">{count}</h1>
+        <div className="mt-4">
+          <button
+            className="common_btn py-2 px-4"
+            onClick={() => setCount(count + 1)}
+          >
+            Click me
+          </button>
+          <button
+            className="common_btn py-2 px-4 ms-3"
+            onClick={() => setCount(count - 1)}
+          >
+            Click me
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CounterPlusMinus;
---
+++ src/views/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CounterPlusMinus from "../components/CounterPlusMinus";
+
+const Main = () => {
+  return (
+    <>
+      <CounterPlusMinus />
+    </>
+  );
+};
+
+export default Main;
